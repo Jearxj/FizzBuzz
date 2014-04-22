d4bbc699e09739bc45945856d7cf6135e1108849
Jearranai Jujaroen <jear.jujaroen@gmail.com>
2014-04-22 13:33:39 -0400

    Moved if/else alerts into while loop

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
  var number = prompt("Please enter a number from 1 - 100");
   console.log("prompt is working");
-    if (number < 1) {
-        alert("Please enter a number greater than 1!");
-    } else if (number > 100) {
-        alert("Please enter a number less than 100!");
-    }
     while (number < 1 || number > 100) {
         var number = prompt("Please enter a number from 1 - 100");
+        if (number < 1) {
+            alert("Please enter a number greater than 1!");
+        } else if (number > 100) {
+            alert("Please enter a number less than 100!");
+        }
     }
-
+ 
  for (var i = 1; i <= number; i++) {
-    console.log(i);
+     console.log(i);
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz");
             $(".container").append("fizzbuzz ");
